8345edaebe8738fe209af377b51faa05ec126fcd
jiajian <jiajianli@futunn.com>
2022-08-16 15:52:49 +0800

    feat: add vite-vue3

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  // esbuild: {
+  //   jsxInject: `import React from 'react'`
+  // },
   resolve: {
     alias: {
-      "@styles": "/src/styles",
+      '@styles': '/src/styles'
+    }
+  },
+  build: {
+    rollupOptions: {
+      plugins: []
     }
   }
 })
